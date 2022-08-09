1eadcd59523fa370bd168da4cec6f5a4831f9894
Thomas Mok <42684333+tomm13@users.noreply.github.com>
2022-08-09 02:08:21 +0100

    V10 prerelease 2

---
+++ V10 Client.py
@@ -0,0 +1,124 @@
+##9/8/2022
+##Broadcast username as opposed to "new connection made"
+##V10 Client
+
+import socket
+import time
+from threading import Thread
+from guizero import *
+
+global s
+s = socket.socket()
+
+global Host
+Host = 0
+
+global Port
+Port = 0
+
+Connected = False
+
+def SendToServer():
+    Message = MessageInput.value
+    if Message:
+        Message = Username + ": " + Message
+        s.send(Message.encode())
+        
+        MessageInput.clear()
+
+def AlwaysUpdate():
+    while True:
+        Message = s.recv(1024).decode()
+        History.text_color = "white"
+        History.append(Message)
+
+def Connect():
+    global Connected
+    try:
+        global Username
+
+##        Username = "tomm"
+        Username = str(UsernameInput.value)
+
+        if Username == "" or Username == "Username":
+            Status.value = "Invalid Username"
+            Status.text_color = "yellow"
+
+        else:
+            try:
+##                Host = HostInput.value
+##                Port = int(PortInput.value, base=10)
+
+                Host = '192.168.1.138'
+                Port = 1234
+                
+                s.connect((Host, Port))
+
+                Connected = True
+
+                s.send(Username.encode())
+
+                Chat()
+            
+            except ConnectionRefusedError:
+                Connected = False
+
+                Status.value = "Connection Full"
+                Status.text_color = "yellow"
+
+    except:        
+        Connected = False
+
+        Status.value = "Connection Failed"
+        Status.text_color = "red"
+
+def Chat():
+    global Chatroom
+    Chatroom = Window(ConnectWindow, height = 700, width = 600, title = "Chatroom")
+    Chatroom.font = "San Francisco Bold"
+    
+    global History
+    History = TextBox(Chatroom, text = "Welcome to this chat room", height = "fill", width = 200, multiline = True, scrollbar = True, align = "top")
+    
+    History.text_size = 16
+
+    global MessageInput
+    MessageInput = TextBox(Chatroom, align = "bottom")
+    
+    SendButton = PushButton(Chatroom, text = "Send", command = SendToServer, align = "bottom")
+    
+    Chatroom.show()
+
+    ConnectWindow.hide()
+
+    ListeningThread = Thread(target = AlwaysUpdate)
+    ListeningThread.start()
+   
+def main():
+    global ConnectWindow
+    ConnectWindow = App(title = "Connect to server", height = 150, width = 250)
+    ConnectWindow.font = "San Francisco Bold"
+
+    global Status
+    Status = Text(ConnectWindow, text = "Not Connected", align = "top")
+    Status.text_size = 18
+    Status.text_color = "red"
+
+    global PortInput, HostInput
+    PortInput = TextBox(ConnectWindow, text = "Port (only be changed in code)", align = "bottom")
+    HostInput = TextBox(ConnectWindow, text = "Host IP (only be changed in code)", align = "bottom")
+
+    global UsernameInput
+    UsernameInput = TextBox(ConnectWindow, text = "Username", align = "bottom")
+
+    AttemptConnect = PushButton(ConnectWindow, text = "Connect", command = Connect, align = "bottom")
+    
+    ConnectWindow.display()
+
+main()
+
+
+
+
+        
+
---
+++ V10 Server.py
@@ -0,0 +1,60 @@
+##9/8/2022
+##V10 Server
+
+import socket
+from threading import Thread
+
+#UserCount = int(input("Enter maximum number of users"))
+UserCount = 2
+UserOnline = 0
+
+HostName = socket.gethostname()
+
+IP = '192.168.1.138'
+Port = 1234
+
+s = socket.socket()
+s.bind((IP, Port))
+print("[Server] Server Hosted with " + str(UserCount) + " space(s)")
+
+s.listen()
+
+global Clients
+Clients = []
+
+global Users
+Users = []
+
+def Broadcast(Message):
+    print("[Client] " + Message)
+    for Client in Clients:
+        Client.send(Message.encode())
+
+def Listen(ClientSocket):
+    while True:
+        Message = ClientSocket.recv(1024).decode()
+        if Message:
+            Broadcast(Message)
+
+for i in range(UserCount):
+    global ClientSocket
+    ClientSocket, Address = s.accept()
+    Clients.append(ClientSocket)
+
+    Username = ClientSocket.recv(1024).decode()
+    Users.append(Username)
+
+    Message = Username + " has connected"
+    Broadcast(Message)
+
+    UserOnline += 1
+    print("[Server] " + str(UserOnline) +
+          " of " + str(UserCount) +
+          " space(s) are taken")
+
+    ListeningThread = Thread(target = Listen, args = [ClientSocket])
+    ListeningThread.start()
+
+
+
+
