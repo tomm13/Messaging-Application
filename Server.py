7050e4278fd29e4aa9c0fef5454b54a1b69e9c90
Thomas Mok <42684333+tomm13@users.noreply.github.com>
2022-08-13 22:13:01 +0100

    Release Candidate 6
    Fully working RSA
    
    Signed-off-by: Thomas Mok <42684333+tomm13@users.noreply.github.com>

--- Server.py
+++ Server.py
@@ -1,10 +1,11 @@
-##12/8/2022
-##attempt at making server more independent
-##V12 RC 3
+##13/8/2022
+##RSA encrpytion and decryption
+##V12 RC 6
 
 import socket
 from threading import Thread
 import time
+import random
 
 #UserCount = int(input("Enter maximum number of users"))
 UserCount = 1000
@@ -16,6 +17,85 @@ IP = '192.168.1.138'
 Ports = [1234, 5023, 5050]
 Clients = []
 Users = []
+
+def GetKeys():
+    global Minimum, Maximum
+    Minimum = 10
+    Maximum = 100
+    Primes = []
+    PrimeCandidates = []
+    for Number in range(Minimum, Maximum):
+        IsPrime = True
+        for Factor in range(2, Number):
+            if Number % Factor == 0:
+                IsPrime = False
+        if IsPrime:
+            Primes.append(Number)
+
+    for Prime in range(2):
+        Max = len(Primes) - 1
+        RandomNum = random.randint(0, Max)
+        PrimeCandidates.append(Primes[RandomNum])
+        Primes.pop(RandomNum)
+
+    global P, Q, N, PhiN
+    P = PrimeCandidates[0]
+    Q = PrimeCandidates[1]
+    N = P * Q
+    PhiN = (P - 1) * (Q - 1)
+
+    eList = []
+    for eCandidate in range(2, PhiN):
+        eList.append(eCandidate)
+
+    global e
+
+    for e in eList:
+        PhiNFactors = []
+        NFactors = []
+        eFactors = []
+        for Factor in range(1, e + 1):
+            if e % Factor == 0:
+                eFactors.append(Factor)
+
+        for Factor in range(1, N + 1):
+            if N % Factor == 0:
+                NFactors.append(Factor)
+
+        for Factor in range(1, PhiN + 1):
+            if PhiN % Factor == 0:
+                PhiNFactors.append(Factor)
+
+        if 1 in eFactors and 1 in PhiNFactors and 1 in NFactors:
+            PhiNFactors.remove(1)
+            NFactors.remove(1)
+            eFactors.remove(1)
+
+        if len(eFactors) == 1 and eFactors[0] not in PhiNFactors and eFactors[0] not in NFactors:
+            print("[Server] Public Key =", (e, N))
+            break
+
+    for k in range(1, 2 * Maximum):
+        if (k * PhiN + 1 ) % e == 0:
+            if not (k * PhiN + 1) // e == e:
+                global d
+                d = (k * PhiN + 1) // e
+                break
+
+    print("[Private] Private Key =", (d, N))
+
+    main()
+
+def RSAEncrypt(Message):
+    RSAEncryptedMessage = []
+    for Letter in Message:
+        Index = ord(Letter)
+        NewIndex = pow(Index, e, N)
+        RSAEncryptedMessage.append(str(NewIndex))
+        RSAEncryptedMessage.append(" ")
+
+    Message = str("".join(RSAEncryptedMessage))
+    return Message
 
 def GeneratePort():
     for PortTest in Ports:
@@ -33,6 +113,8 @@ def GeneratePort():
 def Broadcast(Message):
     time.sleep(0.1)
     print("[Client] " + Message)
+
+    Message = RSAEncrypt(Message)
     for Client in Clients:
         Client.send(Message.encode())
 
@@ -40,6 +122,8 @@ def ServerBroadcast(Message):
     time.sleep(0.1)
     Message = "[Server] " + Message
     print(Message)
+
+    Message = RSAEncrypt(Message)
     for Client in Clients:
         Client.send(Message.encode())
 
@@ -88,7 +172,6 @@ def Command(Message):
                     ServerBroadcast((User))
                     break
                 ServerBroadcast((User + ", "))
-
     else:
         ServerBroadcast("Unknown Command")
 
@@ -112,7 +195,4 @@ def main():
         ListeningThread = Thread(target = Listen, args = [ClientSocket])
         ListeningThread.start()
 
-main()
-
-
-
+GetKeys()
