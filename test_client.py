8de21f3e82e13767aeb357a2b0f009570a695c8c
Thomas Mok <42684333+tomm13@users.noreply.github.com>
2023-01-02 06:02:42 +0000

    Update test_client.py
    Signed-off-by: Thomas Mok <42684333+tomm13@users.noreply.github.com>

--- test_client.py
+++ test_client.py
@@ -1,19 +1,37 @@
-import unittest
-import client
+import pytest
+import client, server, socket
 
-class TestPrototype(unittest.TestCase):
+def test_setup():
+    client.uiInstance.openSetup()
+    client.uiInstance.requestInput(True)
 
-    @classmethod
-    def setUpClass(cls):
-        cls.animationInstance = client.Animation()
-        cls.communicationInstance = client.Communication()
-        cls.connectionInstance = client.Connection()
-        cls.uiInstance = client.UI()
+def test_default_color():
+    assert client.uiInstance.animationColor == (173, 216, 230)
+    assert client.uiInstance.animationColor[0:2]
 
-    def test_inputs(self):
-        self.uiInstance.openSetup()
+def test_inputs():
+    client.connectionInstance.username = "tomm"
+    client.connectionInstance.color = "lightblue"
+    client.connectionInstance.host = socket.gethostbyname(socket.gethostname())
+    client.connectionInstance.port = "49125"
+    client.connectionInstance.publicKey = "817351935383"
+    client.connectionInstance.privateKey = "138055935383"
+    client.connectionInstance.encryptedCipherKey = "441945"
 
-        self.assertEqual('10', '10')
+    client.connectionInstance.hasUsername = True
+    client.connectionInstance.hasColor = True
+    client.connectionInstance.hasHost = True
+    client.connectionInstance.hasPort = True
+    client.connectionInstance.hasPublicKey = True
+    client.connectionInstance.hasPrivateKey = True
+    client.connectionInstance.hasCipherKey = True
 
-#if __name__ == '__main__':
-    #unittest.main()c
+    assert len(client.connectionInstance.port) == 5
+    assert len(client.connectionInstance.publicKey) == 12
+    assert len(client.connectionInstance.privateKey) == 12
+    assert client.connectionInstance.publicKey[6:12] == client.connectionInstance.privateKey[6:12]
+
+def test_joining_failure():
+    client.connectionInstance.connect()
+
+    assert client.connectionInstance.connected == False
