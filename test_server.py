e107826770573a7b57d60ade3e29470a9eb79c87
Thomas Mok <42684333+tomm13@users.noreply.github.com>
2023-01-04 13:24:55 +0000

    Improve tests

--- test_server.py
+++ test_server.py
@@ -1,15 +1,17 @@
-# 2/1/2023
+# 4/1/2023
 # V13.3
 
 import server
 
 # Security
 
+
 def test_generate_port():
     server.securityInstance.generatePort()
 
     assert len(str(server.connectionInstance.port)) == 5
     assert server.connectionInstance.host != '127.0.0.1'
+
 
 def test_generate_key():
     server.securityInstance.generateKey()
@@ -19,14 +21,14 @@ def test_generate_key():
     assert len(str(server.securityInstance.N)) == 6
     assert 1 <= server.securityInstance.cipherKey <= 26
 
+# Test algorithmic accuracy
+
+
 def test_key_retrieval():
     key = 9
     assert key == server.securityInstance.rsaDecrypt(server.securityInstance.rsaEncrypt(key))
 
-def test_string_retrieval():
-    message = "my name is tomm"
-    assert message == server.securityInstance.caesarDecrypt(server.securityInstance.caesarEncrypt(message))
 
-def test_number_retrieval():
-    message = "123456789"
+def test_string_retrieval():
+    message = "my name is tomm 12345"
     assert message == server.securityInstance.caesarDecrypt(server.securityInstance.caesarEncrypt(message))
